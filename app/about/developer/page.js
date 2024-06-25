1e650aa6d0676747f0131ede544702668a6c2be2
Sachinshekharpatel <heroft7024@gmail.com>
2024-06-25 13:47:15 +0530

    Link added in about page

--- app/about/developer/page.js
+++ app/about/developer/page.js
@@ -4,6 +4,9 @@ const page = () => {
   return (
     <div>
       <h1>developer subfolder</h1>
+      <Link to="/about/1">Yash</Link>
+      <Link to="/about/2">Vaibhav</Link>
+      <Link to="/about/3">Suresh</Link>
     </div>
   );
 };
--- app/about/page.js
+++ app/about/page.js
@@ -1,13 +1,19 @@
-"use client";
 import React from "react";
-
+import Link from "next/link";
 const About = () => {
-  const details = [
-    { id: 1, name: "Yash", role: "Senior Developer" },
-    { id: 2, name: "Vaibhav", role: "Backend Developer" },
-    { id: 3, name: "Suresh", role: "Frontend Developer" },
-  ];
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <div>
+        <Link className="cursor-pointer px-1 border-2 border-gray-800" href="/about/1">Yash</Link>
+        <br></br>
+        <Link className="cursor-pointer border-gray-800 px-1 border-2" href="/about/2">Vaibhav</Link>
+        <br></br>
+        <Link className="cursor-pointer border-gray-800 px-1 border-2" href="/about/3">Suresh</Link>
+        <br></br>
+      </div>
+    </div>
+  );
 };
 
 export default About;
